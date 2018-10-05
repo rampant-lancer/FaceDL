b1e35166695e7b5bf5f7fc4ef0b29dcbbb95c99f
md sarfarazul haque <haquemdsarfarazul@gmail.com>
2018-10-05 20:20:49 +0530

    Update preprocess.py

--- Gender/preprocess.py
+++ Gender/preprocess.py
@@ -3,9 +3,9 @@ from keras.utils import to_categorical
 import numpy as np
 
 
-def get_preprocessed_data(n_examples, mode, n_classes):
+def get_preprocessed_data(n_examples, mode, n_classes, shape):
     face = UTKFace.UTKFace()
-    X, Y = face.load_data(n_examples, mode)
+    X, Y = face.load_data(n_examples, mode, shape)
     X = X.astype(np.float16) / 255.0
     Y = to_categorical(Y, num_classes=n_classes)
 
