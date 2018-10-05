220834fc0317ca75848934f2d972016771b1184b
md sarfarazul haque <haquemdsarfarazul@gmail.com>
2018-10-05 14:43:25 +0530

    Update train.py

--- Gender/train.py
+++ Gender/train.py
@@ -1,7 +1,7 @@
 from preprocess import get_preprocessed_data
 from models import get_model 
 from keras.callbacks import ModelCheckpoint
-
+import pickle
 
 n_examples = -1
 mode = 1
@@ -21,4 +21,8 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 filepath="models/model-{epoch:02d}h5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True)
 callbacks_list = [checkpoint]
-model.fit(X, Y, validation_split=0.33, epochs=n_epochs, batch_size=n_batches, callbacks=callbacks_list, verbose=0)
+history = model.fit(X, Y, validation_split=0.33, epochs=n_epochs, batch_size=n_batches, callbacks=callbacks_list, verbose=0)
+with open('history_train', 'wb') as f:
+  pickle.dump(history.history, f)
+  
+  
